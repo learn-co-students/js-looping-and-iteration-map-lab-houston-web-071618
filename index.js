ee6bb977a3c8ce9d93fe0940f46254462f93a511
RebGov <becci.govert@gmail.com>
2018-08-27 15:36:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(driver){
+    return driver.toLowerCase();
+  });
+}
+function nameToAttributes (collection) {
+  return collection.map(function(name) {
+    const nameFirst = name.split(' ')[0];
+    const nameLast = name.split(' ')[1];
+    return {firstName: nameFirst, lastName: nameLast };
+
+  })
+}
+
+function attributesToPhrase (collection) {
+  return collection.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  })
+
+}
