86971ccf36262d4aa69c1c7cbaecb30bdd72f3a4
ELehotzky <Lehotzky.Erica@gmail.com>
2018-08-31 10:36:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file.
+
+const drivers = [
+        { name: 'Bobby',   hometown: 'Pittsburgh'  },
+        { name: 'Sammy',   hometown: 'New York'    },
+        { name: 'Sally',   hometown: 'Cleveland'   },
+        { name: 'Annette', hometown: 'Los Angeles' },
+        { name: 'Bobby',   hometown: 'Tampa Bay'   }
+      ];
+
+function lowerCaseDrivers(drivers) {
+	const lower = drivers.map(function(el) {
+		return el.toLowerCase();
+	});
+	return lower;
+}
+
+function nameToAttributes(drivers) {
+	return drivers.map(function(el) {
+		let firstName = el.split(" ")[0];
+		let lastName = el.split(" ")[1];
+		
+		return {firstName, lastName}
+	});
+}
+
+function attributesToPhrase(drivers) {
+	return drivers.map(function(el) {
+		return `${el.name} is from ${el.hometown}`
+	});
+}
+
+
+
+
